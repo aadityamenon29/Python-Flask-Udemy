03eab7931d540e07048b47236a88a730cf693dd5
aadityamenon29@gmail.com <aadityamenon29@gmail.com>
2017-05-17 03:15:05 -0400

    Authentication and login
    Security features added and tested using POSTMAN
    - auth endpoint automatically created. returns a key
    - use this key to get a specific item, get method is decorated by @jwt
    - use POSTMAN to add header while calling get
    - header is 'Authentication' : 'JWT (key)'

--- app1.py
+++ app1.py
@@ -2,11 +2,15 @@
 from __future__ import unicode_literals
 from flask import Flask, request
 from flask_restful import Resource, Api
-
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
 api = Api(app)
 
+app.secret_key = 'adi'
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
@@ -14,13 +18,14 @@ items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
---
+++ user.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+class User:
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
